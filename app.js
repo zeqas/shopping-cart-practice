57c4c87ce1856a46a18bc3fb82d27e94e2350004
zeqas <casperi52400@gmail.com>
2021-11-10 20:45:57 +0800

    feat: method-override for http methods

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
+const createError = require('http-errors');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const app = express();
+const port = process.env.PORT || 3000
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(session({
-  secret: 'ac',
-  name: 'ac',
-  cookie: { maxAge: 80000 },
-  resave: false,
-  saveUninitialized: true,
-}));
-
 app.set('view engine', 'hbs');
 // view engine setup
 app.engine('.hbs', exphbs({
@@ -29,3 +24,40 @@ app.engine('.hbs', exphbs({
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'ac',
+  name: 'ac',
+  cookie: { maxAge: 80000 },
+  resave: false,
+  saveUninitialized: true,
+}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+module.exports = app;
