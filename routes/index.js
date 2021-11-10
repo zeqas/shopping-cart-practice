2c43f3d032dfe3fe0441ec8bda11a1e8fc1ebf69
zeqas <casperi52400@gmail.com>
2021-11-10 20:49:08 +0800

    feat: CRUD of cart items

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/products', productController.getProducts)
-
 router.get('/cart', cartController.getCart)
-
 router.post('/cart', cartController.postCart)
+// 購物車商品CRUD
+router.post('/cartItem/:id/add', cartController.addCartItem)
+router.post('/cartItem/:id/sub', cartController.subCartItem)
+router.delete('/cartItem/:id', cartController.deleteCartItem)
 
 module.exports = router;
