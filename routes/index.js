8ac5b1d8ab6f78567d3634d5baa811e1d21d5a76
zeqas <casperi52400@gmail.com>
2021-11-11 00:36:18 +0800

    feat: GET orders

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const productController = require('../controllers/productController.js')
 const cartController = require('../controllers/cartController.js')
+const orderController = require('../controllers/orderController.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,5 +17,7 @@ router.post('/cart', cartController.postCart)
 router.post('/cartItem/:id/add', cartController.addCartItem)
 router.post('/cartItem/:id/sub', cartController.subCartItem)
 router.delete('/cartItem/:id', cartController.deleteCartItem)
+
+router.get('/orders', orderController.getOrders)
 
 module.exports = router;
